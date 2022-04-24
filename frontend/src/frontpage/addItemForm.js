c6f91258580b296e0c225c2406ae5845378549fa
Steven <stevenmai1111@hotmail.com>
2022-04-24 14:11:01 +1000

    half added remove button

--- frontend/src/frontpage/addItemForm.js
+++ frontend/src/frontpage/addItemForm.js
@@ -1,23 +1,39 @@
 import "./frontpage.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { render } from "@testing-library/react";
+import { renderIntoDocument } from "react-dom/test-utils";
 
 export default function AddItemForm(props) {
-  const myInput = <input type="Text" placeholder="Hello" />;
+  const myInput = (index) => (
+    <>
+      <input id={index} type="Text" placeholder="Hello" />
+      <button id={index} type="button" onClick={removeLastInput}>{index}</button>
+    </>
+  );
 
-  // Add new input elements in the form
-  const [renderedInputs, setRenderedInputs] = useState([myInput]);
+  // Add or remove input elements in the form
+  const [renderedInputs, setRenderedInputs] = useState([{inputElement: myInput(0), id: 0}]);
 
   const addNewInput = function (e) {
     e.preventDefault();
     let newInputs = [...renderedInputs];
-    newInputs.push(myInput);
+    newInputs.push(myInput(newInputs.length));
     setRenderedInputs(newInputs);
-    console.log("test");
   };
+
+  function removeLastInput(e) {
+    e.preventDefault();
+     newInputs = [...renderedInputs];
+    const newInputs = renderedInputs.filter((item) => item.id !== e.target.id);
+    console.log("test");
+    setRenderedInputs(newInputs);
+  };
+
+  const renderToJsx = () => renderedInputs.map((item) => item.inputElement);
 
   return (
     <form className="form-container">
-      {renderedInputs}
+      {renderToJsx}
       <button
         className="circle-button"
         onClick={addNewInput}
