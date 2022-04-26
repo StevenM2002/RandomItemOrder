ba5605855566356b2608271c9ed017512182a68e
Steven <stevenmai1111@hotmail.com>
2022-04-27 03:28:59 +1000

    a

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ import GetRandomPositions from "./randomPositionsFunctionality/getRandomPosition
 
 // Setup app
 const app = express();
-const PORT = 4000;
+const PORT = 5000;
 app.listen(PORT, () => console.log(`http://localhost:${PORT}`));
 
 // Middleware
